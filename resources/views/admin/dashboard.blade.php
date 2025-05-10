9f5b83048c18a9b7d496c01fbb33b48601ced604
Faiaz <njfaiaz@gmail.com>
2025-05-10 21:19:59 +0600

    Admin Dashboard Chart Update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -55,7 +55,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card widget_2 big_icon email">
+                    <div class="card widget_2 big_icon">
                         <div class="body">
                             <h6>All Inactive User</h6>
                             <h2>{{ $inactiveUserCount }} <small class="info">of {{ $totalUsers }}</small></h2>
@@ -68,7 +68,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="card widget_2 big_icon domains">
+                    <div class="card widget_2 big_icon">
                         <div class="body">
                             <h6>Profile Complete All User </h6>
                             <h2>{{ $profile }} <small class="info">of {{ $activeUserCount }}</small></h2>
@@ -81,114 +81,94 @@
                     </div>
                 </div>
             </div>
-            <div class="row clearfix">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="header">
-                            <h2><strong><i class="zmdi zmdi-chart"></i> Sales</strong> Report</h2>
-                            <ul class="header-dropdown">
-                                <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle"
-                                        data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i
-                                            class="zmdi zmdi-more"></i> </a>
-                                    <ul class="dropdown-menu dropdown-menu-right slideUp">
-                                        <li><a href="javascript:void(0);">Edit</a></li>
-                                        <li><a href="javascript:void(0);">Delete</a></li>
-                                        <li><a href="javascript:void(0);">Report</a></li>
-                                    </ul>
-                                </li>
-                                <li class="remove">
-                                    <a role="button" class="boxs-close"><i class="zmdi zmdi-close"></i></a>
-                                </li>
-                            </ul>
+
+            <div class="body_scroll">
+
+                <div class="container-fluid">
+                    <div class="row clearfix">
+                        <div class="col-lg-12 col-md-12">
+                            <div class="card">
+                                <div class="header">
+                                    <h2><strong>All </strong> District Data</h2>
+
+                                </div>
+                                <div class="body">
+                                    <div id="chart-bar" style="height: 16rem"></div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="body mb-2">
-                            <div class="row clearfix">
-                                <div class="col-lg-3 col-md-6 col-sm-6">
-                                    <div class="state_w1 mb-1 mt-1">
-                                        <div class="d-flex justify-content-between">
-                                            <div>
-                                                <h5>2,365</h5>
-                                                <span><i class="zmdi zmdi-balance"></i> Revenue</span>
-                                            </div>
-                                            <div class="sparkline" data-type="bar" data-width="97%" data-height="55px"
-                                                data-bar-Width="3" data-bar-Spacing="5" data-bar-Color="#868e96">
-                                                5,2,3,7,6,4,8,1</div>
-                                        </div>
-                                    </div>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="header">
+                                    <h2><strong>Blood </strong> Group</h2>
+
                                 </div>
-                                <div class="col-lg-3 col-md-6 col-sm-6">
-                                    <div class="state_w1 mb-1 mt-1">
-                                        <div class="d-flex justify-content-between">
-                                            <div>
-                                                <h5>365</h5>
-                                                <span><i class="zmdi zmdi-turning-sign"></i> Returns</span>
-                                            </div>
-                                            <div class="sparkline" data-type="bar" data-width="97%" data-height="55px"
-                                                data-bar-Width="3" data-bar-Spacing="5" data-bar-Color="#2bcbba">
-                                                8,2,6,5,1,4,4,3</div>
+                                <div class="body">
+                                    <div class="row clearfix">
+                                        <div class="col-lg-6 col-md-12">
+                                            <div id="chart-donut" style="height: 17rem"></div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-3 col-md-6 col-sm-6">
-                                    <div class="state_w1 mb-1 mt-1">
-                                        <div class="d-flex justify-content-between">
-                                            <div>
-                                                <h5>65</h5>
-                                                <span><i class="zmdi zmdi-alert-circle-o"></i> Queries</span>
+                                        <div class="col-lg-6 col-md-12">
+                                            <div class="table-responsive">
+                                                <table class="table table-hover c_table mb-0">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1</td>
+                                                            <td>AB+</td>
+                                                            <td>6985 <i class="zmdi zmdi-caret-up text-success"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2</td>
+                                                            <td>AB_</td>
+                                                            <td>2697 <i class="zmdi zmdi-caret-up text-success"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>3</td>
+                                                            <td>A+</td>
+                                                            <td>3597 <i class="zmdi zmdi-caret-down text-danger"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>4</td>
+                                                            <td>A-</td>
+                                                            <td>2145 <i class="zmdi zmdi-caret-up text-success"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>5</td>
+                                                            <td>B+</td>
+                                                            <td>54 <i class="zmdi zmdi-caret-down text-danger"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>6</td>
+                                                            <td>B-</td>
+                                                            <td>54 <i class="zmdi zmdi-caret-down text-danger"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>7</td>
+                                                            <td>O+</td>
+                                                            <td>54 <i class="zmdi zmdi-caret-down text-danger"></i>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>8</td>
+                                                            <td>O-</td>
+                                                            <td>54 <i class="zmdi zmdi-caret-down text-danger"></i>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
                                             </div>
-                                            <div class="sparkline" data-type="bar" data-width="97%" data-height="55px"
-                                                data-bar-Width="3" data-bar-Spacing="5" data-bar-Color="#82c885">
-                                                4,4,3,9,2,1,5,7</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-3 col-md-6 col-sm-6">
-                                    <div class="state_w1 mb-1 mt-1">
-                                        <div class="d-flex justify-content-between">
-                                            <div>
-                                                <h5>2,055</h5>
-                                                <span><i class="zmdi zmdi-print"></i> Invoices</span>
-                                            </div>
-                                            <div class="sparkline" data-type="bar" data-width="97%" data-height="55px"
-                                                data-bar-Width="3" data-bar-Spacing="5" data-bar-Color="#45aaf2">
-                                                7,5,3,8,4,6,2,9</div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="body">
-                            <div id="chart-area-spline-sracked" class="c3_chart d_sales"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="row clearfix">
-
-                <div class="col-lg-4 col-md-12">
-                    <div class="card">
-                        <div class="header">
-                            <h2><strong>Distribution</strong></h2>
-                            <ul class="header-dropdown">
-                                <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle"
-                                        data-toggle="dropdown" role="button" aria-haspopup="true"
-                                        aria-expanded="false"> <i class="zmdi zmdi-more"></i> </a>
-                                    <ul class="dropdown-menu dropdown-menu-right slideUp">
-                                        <li><a href="javascript:void(0);">Edit</a></li>
-                                        <li><a href="javascript:void(0);">Delete</a></li>
-                                        <li><a href="javascript:void(0);">Report</a></li>
-                                    </ul>
-                                </li>
-                                <li class="remove">
-                                    <a role="button" class="boxs-close"><i class="zmdi zmdi-close"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="body text-center">
-                            <div id="chart-pie" class="c3_chart d_distribution"></div>
-                            <button class="btn btn-primary mt-4 mb-4">View More</button>
                         </div>
                     </div>
                 </div>
@@ -200,6 +180,7 @@
         <script src="{{ asset('assets/admin/bundles/jvectormap.bundle.js') }}"></script> <!-- JVectorMap Plugin Js -->
         <script src="{{ asset('assets/admin/bundles/sparkline.bundle.js') }}"></script> <!-- Sparkline Plugin Js -->
         <script src="{{ asset('assets/admin/bundles/c3.bundle.js') }}"></script>
+        <script src="{{ asset('assets/admin/js/pages/blog/blog.js') }}"></script>
         <script src="{{ asset('assets/admin/js/pages/index.js') }}"></script>
     @endpush
 @endsection
